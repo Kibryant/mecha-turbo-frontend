0d64b2063ed196704add77b52675c59be963dd27
Kibryant <arthurgustavon@gmail.com>
2024-09-14 12:12:26 -0300

    change logic of filter

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 export const api = Axios.create({
-  baseURL: process.env.EXPO_PUBLIC_BACKEND_URL,
+  baseURL: process.env.EXPO_PUBLIC_BACKEND_URL || "http://localhost:3333",
   headers: {
     "Content-Type": "application/json",
   },
