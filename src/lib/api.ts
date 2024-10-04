41c4bf51ae25ce14eb32985381d408f3ed6c67fc
Kibryant <arthurgustavon@gmail.com>
2024-10-04 17:47:12 -0300

    fix

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 export const api = Axios.create({
-  baseURL: process.env.EXPO_PUBLIC_BACKEND_URL || "http://localhost:3333",
+  baseURL: "https://mecha-turbo-backend.vercel.app",
   headers: {
     "Content-Type": "application/json",
   },
