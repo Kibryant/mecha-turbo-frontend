431162a842001e967593cc35c8fba659be1d8aac
Kibryant <arthurgustavon@gmail.com>
2024-10-08 09:47:39 -0300

    fix

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -2,12 +2,12 @@ import { z } from "zod";
 
 export const envSchema = z.object({
   EXPO_PUBLIC_BACKEND_URL: z.string().url(),
-  API_KEY: z.string(),
-  AUTH_DOMAIN: z.string(),
-  PROJECT_ID: z.string(),
-  STORAGE_BUCKET: z.string(),
-  MESSAGING_SENDER_ID: z.string(),
-  APP_ID: z.string(),
+  EXPO_PUBLIC_API_KEY: z.string(),
+  EXPO_PUBLIC_AUTH_DOMAIN: z.string(),
+  EXPO_PUBLIC_PROJECT_ID: z.string(),
+  EXPO_PUBLIC_STORAGE_BUCKET: z.string(),
+  EXPO_PUBLIC_MESSAGING_SENDER_ID: z.string(),
+  EXPO_PUBLIC_APP_ID: z.string(),
 });
 
 export type Env = z.infer<typeof envSchema>;
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getStorage } from "firebase/storage";
 import { env } from "./env";
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: env.API_KEY,
-  authDomain: env.AUTH_DOMAIN,
-  projectId: env.PROJECT_ID,
-  storageBucket: env.STORAGE_BUCKET,
-  messagingSenderId: env.MESSAGING_SENDER_ID,
-  appId: env.APP_ID,
+  apiKey: env.EXPO_PUBLIC_API_KEY,
+  authDomain: env.EXPO_PUBLIC_AUTH_DOMAIN,
+  projectId: env.EXPO_PUBLIC_PROJECT_ID,
+  storageBucket: env.EXPO_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
+  appId: env.EXPO_PUBLIC_APP_ID,
 };
 
 export const firebase = initializeApp(firebaseConfig);
